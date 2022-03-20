b94e00c96390e0a38d293716af967325afabb09c
Lucía23 <lucblareg@gmail.com>
2022-03-20 19:31:07 +0100

    Primer commit

---
+++ src/main/java/com/mycompany/ejerc10/Sumador.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.ejerc10;
+
+public class Sumador {
+
+    public static float sumar(float a, float b) {
+        return a + b;
+    }
+}
+
